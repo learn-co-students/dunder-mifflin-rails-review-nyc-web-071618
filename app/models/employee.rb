b0de3e26c6da7af93d8c62108ba5377a4e1eaab5
trisjones <trisjones04@gmail.com>
2018-08-16 15:16:16 -0400

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+
+  # def dog_name=(name)
+  #   self.dog = Dog.find_or_create_by(name: name)
+  # end
+  #
+  # def dog_name
+  #   self.dog ? self.dog.name : nil
+  # end
+
 end
